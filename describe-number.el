40691343f7934cc9b404595bc4325111734a9e9b
Morten Kristensen <msk@nullpointer.dk>
2015-10-26 22:21:49 +0100

    Version 0.2.0

--- describe-number.el
+++ describe-number.el
@@ -5,7 +5,7 @@
 ;; Author: Morten Slot Kristensen <msk AT nullpointer DOT dk>
 ;; Keywords: describe value help
 ;; URL: https://github.com/netromdk/describe-number
-;; Version: 0.1.0
+;; Version: 0.2.0
 
 ;; This program is free software; you can redistribute it and/or modify it under the terms of the
 ;; GNU General Public License as published by the Free Software Foundation, either version 3 of the
